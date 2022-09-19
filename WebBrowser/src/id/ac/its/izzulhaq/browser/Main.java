694317e869f03e489c71acadd030a9f408a836e6
Izzulhaq Fawwaz Syauqiy <57482751+IzzulhaqFS@users.noreply.github.com>
2022-09-19 22:49:04 +0700

    Initiate WebBrowser Repository
    Mengupload file program untuk folder WebBrowser.

---
+++ WebBrowser/src/id/ac/its/izzulhaq/browser/Main.java
@@ -0,0 +1,10 @@
+package id.ac.its.izzulhaq.browser;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Browser browser = new Browser();
+        browser.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
