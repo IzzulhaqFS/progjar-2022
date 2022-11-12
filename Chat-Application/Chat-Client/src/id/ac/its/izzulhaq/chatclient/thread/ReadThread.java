fa3965b9b764fd46e72ea29bba44e0bb006d2720
Izzulhaq Fawwaz Syauqiy <57482751+IzzulhaqFS@users.noreply.github.com>
2022-11-12 16:55:27 +0700

    Update ReadThread.java

--- Chat-Application/Chat-Client/src/id/ac/its/izzulhaq/chatclient/thread/ReadThread.java
+++ Chat-Application/Chat-Client/src/id/ac/its/izzulhaq/chatclient/thread/ReadThread.java
@@ -1,6 +1,7 @@
 package id.ac.its.izzulhaq.chatclient.thread;
 
 import id.ac.its.izzulhaq.chatclient.ChatClient;
+import id.ac.its.izzulhaq.chatclient.des.DES;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,11 +11,9 @@ import java.net.Socket;
 
 public class ReadThread extends Thread {
     private BufferedReader br;
-    private Socket socket;
-    private ChatClient client;
+    private final ChatClient client;
 
     public ReadThread(Socket socket, ChatClient client) {
-        this.socket = socket;
         this.client = client;
 
         try {
@@ -31,6 +30,35 @@ public class ReadThread extends Thread {
         while (true) {
             try {
                 String response = br.readLine();
+                DES decipher = new DES();
+
+                String KEY = "7510F92FC6E8E3DA";
+
+//                String[] splitResponse = response.split(" ");
+                String userName = response.substring(0, response.indexOf(":") - 1);
+                String message = response.substring(response.indexOf(":") + 2);
+
+                if (!userName.equals("Server")) {
+                    int n = message.length();
+                    if (n != 16) {
+                        StringBuilder sb = new StringBuilder();
+                        String temp;
+
+                        for (int i = 0; i < n; i += 16) {
+                            temp = message.substring(i, i + 16);
+                            sb.append(decipher.decrypt(temp, KEY));
+                        }
+
+                        message = sb.toString();
+                    }
+                    else {
+                        message = decipher.decrypt(message, KEY);
+                    }
+                    message = hexToString(message);
+                }
+
+                response = userName + " : " + message;
+
                 System.out.println("\n" + response);
 
                 if (client.getUserName() != null) {
@@ -43,5 +71,16 @@ public class ReadThread extends Thread {
                 break;
             }
         }
+    }
+
+    private String hexToString(String hexStr) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < hexStr.length(); i += 2) {
+            String str = hexStr.substring(i, i + 2);
+            sb.append((char) Integer.parseInt(str, 16));
+        }
+
+        return sb.toString();
     }
 }
